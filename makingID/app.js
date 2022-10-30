9a100abf2f28a55da382443df61cb9515e4dff41
mahfuz-antor <antorcnj2014@gmail.com>
2022-10-31 02:18:58 +0600

    making the unique ID project

---
+++ makingID/app.js
@@ -0,0 +1,16 @@
+const showID = document.getElementById("showID");
+const makingID = document.getElementById("makingIDBtn");
+
+const displayID = (finalID) => {
+  showID.innerHTML = finalID;
+};
+
+let result = 0;
+let preYear = new Date().getFullYear().toString();
+let preMonth = new Date().getMonth() + 1;
+preMonth.toString();
+const makingUniqueID = () => {
+  result = result + 1;
+  let finalID = preYear + preMonth + result;
+  displayID(finalID);
+};
