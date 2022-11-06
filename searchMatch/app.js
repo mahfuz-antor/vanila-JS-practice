c47e8cb07eed934d3ad7ecaa19248a23b49f1c81
mahfuz-antor <antorcnj2014@gmail.com>
2022-11-07 02:39:48 +0600

    search function is added

---
+++ searchMatch/app.js
@@ -0,0 +1,29 @@
+// check the search result from the Array
+
+// Showing the Data on the UI
+const searchField = document.getElementById("searchField");
+const searchResultShow = document.getElementById("searchResult");
+
+const products = [
+  { name: "Samsung Galaxy M10 mobile", price: 12000, available: "Yes" },
+  { name: "Lenovo laptop", price: 45000, available: "Yes" },
+  { name: "Asus MX3 laptop", price: 50000, available: "Yes" },
+  { name: "LG Q9 mobile", price: 15000, available: "Yes" },
+];
+
+// showing the products Array Name
+searchField.innerText = products.map((data) => data.name);
+// search field matches function
+const searchProducts = (products, searchItem) => {
+  const searchResult = [];
+  for (const product of products) {
+    if (product.name.includes(searchItem)) {
+      //   console.log(product, "inside condition");
+      searchResult.push(product);
+    }
+  }
+  return searchResult;
+};
+const getData = searchProducts(products, "laptop");
+// console.log(getData.map((data) => data.name));
+searchResultShow.innerHTML = getData.map((data) => data.name);
