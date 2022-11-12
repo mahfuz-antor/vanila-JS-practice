588d0337dade3a43ede9ca7763876d6e5c32073e
mahfuz-antor <antorcnj2014@gmail.com>
2022-11-12 23:55:02 +0600

    click, focus and blur function created

--- DOM-related/evnetKeyMouse/app.js
+++ DOM-related/evnetKeyMouse/app.js
@@ -1,1 +1,18 @@
 // event, focus, keypress, keydown, keyup, onchange, onblur
+const hideText = document.getElementById("hideText");
+const inputField = document.getElementById("inputField");
+const deleteButton = document.getElementById("deleteBtn");
+
+// click function adding
+deleteButton.addEventListener("click", function () {
+  hideText.style.display = "none";
+});
+
+// focus function adding
+inputField.addEventListener("focus", function () {
+  document.body.style.backgroundColor = "orange";
+});
+// blur function adding
+inputField.addEventListener("blur", function () {
+  document.body.style.backgroundColor = "transparent";
+});
