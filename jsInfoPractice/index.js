4fe14729f935788fb0dc4a9b367da15b166d317e
mahfuz-antor <antorcnj2014@gmail.com>
2023-07-15 11:42:22 +0600

    chaining assignment

--- jsInfoPractice/index.js
+++ jsInfoPractice/index.js
@@ -1,5 +1,11 @@
-let a = 1;
-let b = 2;
-let c = 3 - (a = b + 1);
-console.log(a); // 3
-console.log(c); // 0
+// let a = 1;
+// let b = 2;
+// let c = 3 - (a = b + 1);
+// console.log(a); // 3
+// console.log(c); // 0
+
+// Chaining Assignment
+
+// let a, b, c;
+// a = b = c = 2 + 2;
+// console.log(a, b, c); // 4 4 4
