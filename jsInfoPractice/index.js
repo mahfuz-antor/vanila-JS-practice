45adaa98159f113a3d3940f9844fc61ec192d214
mahfuz-antor <antorcnj2014@gmail.com>
2023-07-18 23:34:46 +0600

    && operator

--- jsInfoPractice/index.js
+++ jsInfoPractice/index.js
@@ -19,3 +19,11 @@
 // console.log("Z" > "A"); // true
 // console.log("Glow" > "Glee"); // true
 // console.log("Bee" > "Be"); // true
+
+// JS && operator
+let hour = 12;
+let minute = 30;
+
+if (hour == 12 && minute == 30) {
+  console.log("The time is 12:30");
+}
